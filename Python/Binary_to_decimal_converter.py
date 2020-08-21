3c1991e8328afdb182a1cf77bcabcf33d45e0580
yadavrajsky <70022991+yadavrajsky@users.noreply.github.com>
2020-08-21 22:43:03 +0530

    Created Binary_to_decimal_converter.py

--- Python/Binary_to_decimal_converter.py
+++ Python/Binary_to_decimal_converter.py
@@ -1,1 +1,12 @@
+def Binary_To_Decimal_converter(binary_number):
+    str_binary_number = str(binary_number)
+    result = 0
+    i = 0
+    for j in str_binary_number[::-1]:
+        result += (2**i) * int(j)
+        i += 1
+    return result
 
+
+c = Binary_To_Decimal_converter(111100)
+print(c)
