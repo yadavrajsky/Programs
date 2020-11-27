e6a0f40cfe9f19435f1abe228ed943427f5c53d1
yadavrajsky <70022991+yadavrajsky@users.noreply.github.com>
2020-11-27 23:22:57 +0530

    Created FirstCode.java
    This is my first program in Java

---
+++ Java/FirstCode.java
@@ -0,0 +1,26 @@
+
+public class FirstCode {
+	
+public static void main(String args[])
+{
+	int num=50_000;//4 bytes
+	short s=1;//2 bytes
+	float dec=5.5f;//4 bytes
+	double percent=5.5;//8 bytes
+	byte b=1;//1 byte
+	long l=500000000l;
+	char c='A';
+	double d1=5;
+	int k=(int)5.6;//type casting
+	c=66;
+	c++;
+	System.out.print("Hello world\n");
+	System.out.print(num+"\n");
+	System.out.print(dec+"\n");
+	System.out.print(percent+"\n");
+	System.out.println(c+"\n");
+	System.out.println(d1+"\n");
+	System.out.println(k+"\n");
+}
+
+}
