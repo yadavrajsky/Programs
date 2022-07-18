57cb867dda2375285c0b713dc736aa89bf1b3bc0
yadavrajsky <70022991+yadavrajsky@users.noreply.github.com>
2022-07-18 15:21:16 +0530

    Variable Arguments

---
+++ Java/Var_argsDemo.java
@@ -0,0 +1,22 @@
+class Var_ags {
+
+  public void show(int ..s)
+  {
+
+    for(int x:s)
+    {
+      System.out.println(x);
+    }
+  }
+}
+
+class Var_agsDemo
+{
+
+  public static void main(String[] args) {
+    Var_ags obj=new Var_ags();
+    obj.show(10);
+    obj.show(10,20);
+    obj.show(10,20,30);
+  }
+}
