64f83756887e4821b23b579c4f806e781bce2c13
yadavrajsky <70022991+yadavrajsky@users.noreply.github.com>
2022-07-18 20:06:44 +0530

    Non-primitive Array in Java

---
+++ Java/non_primitive_array.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Customer {
+    private int acc_no;
+    private String name;
+    private int bal;
+    private int rate = 5;
+    private double i;
+
+    public Customer(int a, String n, int b) {
+        acc_no = a;
+        name = n;
+        bal = b;
+
+    }
+
+    public void getInterest() {
+        i = bal * rate / 100;
+    }
+
+    public void display() {
+        System.out.println("Name : " + name + "Balance: " + bal + "Interest" + i);
+    }
+}
+
+class BankDemo {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("How many customers?");
+        int n = s.nextInt();
+        Customer[] arr = new Customer[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter account number ");
+            int a = s.nextInt();
+            System.out.println("Enter Name ");
+            String s = s.nextLine();
+            System.out.println("Enter Balance");
+            int b = s.nextInt();
+            arr[i] = new Customer(a, s, b);
+        }
+
+        for (Customer c : arr) {
+
+            c.getInterest();
+            c.display();
+        }
+
+    }
+}
