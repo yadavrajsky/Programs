acbd5649db07edbb7c3fa5ee7e3fbb9064993781
yadavrajsky <70022991+yadavrajsky@users.noreply.github.com>
2022-07-18 07:33:36 +0530

    Command line arguments

---
+++ Java/Cla.java
@@ -0,0 +1,8 @@
+class Cli {
+
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        System.out.println("Sum is " + (x + y));
+    }
+}
