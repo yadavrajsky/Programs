508b35e259e4e983436f9ff65748f524a5db63e3
yadavrajsky <70022991+yadavrajsky@users.noreply.github.com>
2022-07-18 09:56:03 +0530

    Updated enhancedForloop_PrimeNumbers.java

--- Java/enhancedForloop_PrimeNumbers.java
+++ Java/enhancedForloop_PrimeNumbers.java
@@ -1,17 +1,13 @@
-class PrimeNumbers
-{
-public static void main(String[] args)
-{
-int a=10,b=20;
-int i;
-outer:for(int j=a;j<=b;j++)
-{
-for(i=2;i<=j-1;i++)
-{
-if(j%i==0)
-continue outer;
-} 
-System.out.println(j+" is Prime Number");
-}
-}
+class PrimeNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int i;
+        outer: for (int j = a; j <= b; j++) {
+            for (i = 2; i <= j - 1; i++) {
+                if (j % i == 0)
+                    continue outer;
+            }
+            System.out.println(j + " is Prime Number");
+        }
+    }
 }
